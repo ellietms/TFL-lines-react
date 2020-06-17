99aa3c9e527e403de2a309a0df19a2ad6b10a727
ellie <ellie.tms1371@gmail.com>
2020-06-17 13:13:17 +0100

    add codes

--- src/Select.js
+++ src/Select.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 const Select = ({dataTfl}) => {
 return(
     <div className="form-group">
-    <select className="form-control my-4" id="exampleFormControlSelect1">
+    <select className="form-control my-4 mx-auto" id="exampleFormControlSelect1">
     <option className=""> Choose a Mode of Transport...</option>    
     {dataTfl.map((eachVehicle,index) => 
     <option key={index} className="text-center">{eachVehicle.modeName}</option>
