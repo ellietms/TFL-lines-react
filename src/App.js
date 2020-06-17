91c4c49bf1ac41944d6628c15018bf1c8683220f
ellie <ellie.tms1371@gmail.com>
2020-06-17 11:22:46 +0100

    add select component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState}from 'react';
+import Select from './Select';
 import './App.css';
 
 function App() {
-  return (
+const [dataTfl,setDataTfl] = useState();
+
+useEffect(()=>{
+  fetch("https://api.tfl.gov.uk/Line/Meta/Modes")
+  .then(res => res.json())
+  .then(data => setDataTfl(data));
+},[]);
+
+console.log(dataTfl);
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Select dataTfl={dataTfl}/>
     </div>
-  );
+);
 }
+
 
 export default App;
---
+++ src/Select.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Select = ({dataTfl}) => {
+
+
+
+}
+
+
+
+export default Select;
