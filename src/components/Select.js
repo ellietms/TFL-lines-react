b17a55e33c46147c04101c872df10572fde1f867
ellie <ellie.tms1371@gmail.com>
2020-06-20 22:42:04 +0100

    Update Select.js

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,26 +1,24 @@
-import React, {useEffect,useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-const Select = ({dataTfl,handleNameVehicle}) => { 
-    return(
+const Select = ({ dataTfl, handleNameVehicle }) => {
+  return (
     <div className="form-group">
-    <select 
-    className="form-control my-4 mb-5 mx-auto"
-    onChange={(event) => { handleNameVehicle(event)}}> 
-    <option> Choose a Mode of Transport...</option>  
-    {dataTfl.map((eachVehicle,index) => 
-    <option 
-    key={index} 
-    className="text-center"> 
-    {eachVehicle.modeName}
-    </option>
-   )
-   }
-    </select>  
-   
-    </div> 
-)
-}
-
+      <select
+        className="form-control my-4 mb-5 mx-auto"
+        onChange={(event) => {
+          handleNameVehicle(event);
+        }}
+      >
+        <option> Choose a Mode of Transport...</option>
+        {dataTfl.map((eachVehicle, index) => (
+          <option key={index} className="text-center">
+            {eachVehicle.modeName}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default Select;
