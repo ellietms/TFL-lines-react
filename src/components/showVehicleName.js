c049d21256a64ff0346c57afe54fc1a7aecb9f6c
ellie <ellie.tms1371@gmail.com>
2020-06-20 22:49:44 +0100

    css

--- src/components/showVehicleName.js
+++ src/components/showVehicleName.js
@@ -4,9 +4,9 @@ const VehicleName = ({line}) => {
   return (
     <p className="nameVehicle ml-2">
       <span
-        className="font-weight-bold
-        text-dark mx-auto p-4 d-flex border border-danger"
-      >
+        className="selectedMode font-weight-bold
+         mx-auto p-4 d-flex"
+      >    
         You selected mode:{line}
       </span>
     </p>
