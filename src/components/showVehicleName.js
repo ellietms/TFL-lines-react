56527d15ec6e0cab43172e9551de328918699c01
ellie <ellie.tms1371@gmail.com>
2020-06-20 22:42:14 +0100

    Update showVehicleName.js

--- src/components/showVehicleName.js
+++ src/components/showVehicleName.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const VehicleName = ({line}) => {
-    console.log(line)
-    return(
-        <p className="nameVehicle ml-2">
-        <span className="font-weight-bold
-        text-dark mx-auto p-4 d-flex border border-danger">
-         You selected mode:{line}
-        </span>
-       </p>
-    )
-}
-
+const VehicleName = ({ line }) => {
+  console.log(line);
+  return (
+    <p className="nameVehicle ml-2">
+      <span
+        className="font-weight-bold
+        text-dark mx-auto p-4 d-flex border border-danger"
+      >
+        You selected mode:{line}
+      </span>
+    </p>
+  );
+};
 
 export default VehicleName;
