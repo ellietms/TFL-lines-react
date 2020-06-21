e4a76238f4494382a66e44ef65ef7c8f7e77412a
ellie <ellie.tms1371@gmail.com>
2020-06-21 10:19:58 +0100

    Update showVehicleName.js

--- src/components/showVehicleName.js
+++ src/components/showVehicleName.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const VehicleName = ({line,nameVehicle}) => {
+const VehicleName = ({ line, nameVehicle }) => {
   return (
     <p className="mx-auto">
       <span
         className="selectedMode font-weight-bold
          mx-auto p-4 d-flex"
-      >    
+      >
         {nameVehicle} - {line}
       </span>
     </p>
