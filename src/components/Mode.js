5c7f0ba1f47b3d0d9ea8d27a0b405e106e44af91
ellie <ellie.tms1371@gmail.com>
2020-06-21 08:21:41 +0100

    first line

--- src/components/Mode.js
+++ src/components/Mode.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import VehicleName from "./showVehicleName";
+import ShowSelectedLine from './showSelectedLine';
 import "bootstrap/dist/css/bootstrap.css";
 
 const Mode = ({ nameVehicle, setNameVehicle }) => {
   const [dataForNewMode, setDataForNewMode] = useState([]);
   const [line, setLine] = useState("");
-
+  console.log(line);
   useEffect(() => {
     fetch(`https://api.tfl.gov.uk/Line/Mode/${nameVehicle}`)
       .then((res) => res.json())
@@ -19,16 +20,18 @@ const Mode = ({ nameVehicle, setNameVehicle }) => {
     if (event.target.value !== "Mode of Transport...") {
       const newLine = event.target.value;
       setLine(newLine);
-      setNameVehicle(nameVehicle);}
+      setNameVehicle(nameVehicle);
+      }
     else {
       if (event.target.value === "Mode of Transport...")
       setLine("please choose your transportation mode");
     }
   }
 
-  let mode;
+  let lineSelected;
+
   if (dataForNewMode.length !== 0) {
-    mode = (
+    lineSelected = (
       <div className="form-group">
         <select
           className="form-control my-4 mb-5 mx-auto"
@@ -37,27 +40,47 @@ const Mode = ({ nameVehicle, setNameVehicle }) => {
           <option>Mode of Transport...</option>
           {dataForNewMode.map((eachVehicle, index) => {
             return (
-              <option key={index} className="text-center">
-                {eachVehicle.id}
+              <option key={index} 
+              className="text-center">
+              {eachVehicle.id}
               </option>
             );
           })}
         </select>
         <VehicleName nameVehicle={nameVehicle} line={line} />
+        { (line !== "") &&
+          <ShowSelectedLine
+          selectedLine={line}
+          nameVehicle={nameVehicle}/>
+        }
+        }
       </div>
     );
   } else {
-    mode = (
+    lineSelected = (
       <div className="form-group">
         <VehicleName
           nameVehicle={nameVehicle}
           line={"Sorry,We do not support this transportation mode"}
         />
+        {(line !== "") &&
+          <ShowSelectedLine
+          selectedLine={line}
+          nameVehicle={nameVehicle}/>
+        }
       </div>
     );
   }
 
-  return <div>{mode}</div>;
+  return(
+  <div>
+    {lineSelected}
+    {/* <ShowSelectedLine
+         selectedLine={line}
+         nameVehicle={nameVehicle}
+    /> */}
+  </div>
+  )
 };
 
 export default Mode;
--- src/components/showSelectedLine.js
+++ src/components/showSelectedLine.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
+const ShowSelectedLine = ({ selectedLine, nameVehicle }) => {
+  const [dataForSelectedLine, setDataForSelectedLine] = useState([]);
+  useEffect(() => {});
+  useEffect(() => {
+    fetch(`https://api.tfl.gov.uk/Line/${selectedLine}/Route`)
+      .then((res) => res.json())
+      .then((data) => setDataForSelectedLine(data.routeSections[0]))
+      .catch((error) => console.log("Sorry" + error.status));
+  }, [selectedLine]);
+  console.log(dataForSelectedLine);
 
-const ShowSelectedLine = () => {
-    
-    return(
-
+  let SelectedLine;
+  if (dataForSelectedLine.length === 0) {
+    return (
+      <div>
+        <div>
+          <div className="card text-white bg-info mb-3">
+            <div className="card-header">
+              {nameVehicle} : {selectedLine}
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">START OF LINE</h5>
+              <p className="card-text">
+                Sorry We do not have any transportation for this line
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     );
-}
-
+  } else {
+    SelectedLine = (
+        <div className="card mb-3 start-line ">
+          <div className="card-header">
+            {nameVehicle} : {selectedLine}
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">START OF LINE</h5>
+            <p className="card-text">{dataForSelectedLine.originationName}</p>
+          </div>
+        </div>
+    );
+  }
+  return <div>{SelectedLine}</div>;
+};
 
 export default ShowSelectedLine;
-
-
-
